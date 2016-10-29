d1a4a182256f9b2a6c8762f64787f37b8415612c
MacBook Pro <macbookpro@Nagarajus-PC.local>
2016-10-29 16:55:21 +0530

    adding gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,63 @@
-var os = require('os');
 var gulp = require('gulp');
-var useref = require('gulp-useref');
-var cache = require('gulp-cache');
 var runSequence = require('run-sequence');
-var open = require('gulp-open');
+var browserSync = require('browser-sync').create();
+var connect = require('connect');
 var clean = require('gulp-clean');
 
 
-gulp.task('hello', function(){
-    console.log('welcome to Pivot-React-Angularjs');
-});
-
-gulp.task('useref', function(){
-  return gulp.src('app/**/*')
-    .pipe(useref())
-    .pipe(gulp.dest('dist'))
-});
-
-
-gulp.task('open', function(){
-  gulp.src('./dist/index.html')
-  .pipe(open());
-});
-
+// Clean
 gulp.task('clean', function () {
-    return gulp.src('dist/', {read: false})
+    return gulp.src('dist/*', {read: false})
         .pipe(clean());
 });
 
+// HTML
+gulp.task('html', function() {
+  return gulp.src('app/*.html')
+  .pipe(gulp.dest('dist'))
+});
+
+// JS
+gulp.task('js', function() {
+  return gulp.src('app/js/*.js')
+  .pipe(gulp.dest('dist/js'))
+});
+
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', [], function() {
+
+    browserSync.init({
+        server: "./dist",
+        port: 9393,
+        ghostMode: false
+    });
+
+    //gulp.watch("app/*.html").on('change', browserSync.reload);
+   
+});
+
+gulp.task('watch', function(){
+  /*  gulp.watch('app/css/*.css').on('change', browserSync.reload);
+    gulp.watch('app/js/*.js').on('change', browserSync.reload);*/
+    //gulp.watch('app/*').on('change', browserSync.reload);
+     gulp.watch('dist/*').on('change', browserSync.reload);
+});
+
 gulp.task('build', function (callback) {
-  runSequence(
-    ['useref',  'open'],
+  runSequence('clean', 
+    ['html', 'js' ],
+    'serve',
+    'watch',
     callback
   )
 });
-
+gulp.task('default', function (callback) {
+  runSequence('clean', 
+    ['html', 'js' ],
+    'serve',
+    'watch',
+    callback
+  )
+});
